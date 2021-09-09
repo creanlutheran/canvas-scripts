ccea83b20ec9d6c4a07676fe7926d6d0a2ec0ca3
creanlutheran <44102805+creanlutheran@users.noreply.github.com>
2021-09-09 08:55:19 -0700

    Update set-default-due-time-v1.0.user.js

--- set-default-due-time-v1.0.user.js
+++ set-default-due-time-v1.0.user.js
@@ -6,22 +6,38 @@
 // @include      https:/*.instructure.com/courses/*/assignments/*
 // @include      https:/*.instructure.com/courses/*/discussion_topics/*
 // @include      https:/*.instructure.com/courses/*/quizzes/*
+// @require      https://unpkg.com/dayjs@1.8.21/dayjs.min.js
+
+
 
 // ==/UserScript==
 (function() {
 
 	$(document).ready(function() {
 
+
 		setTimeout(function() {
 
-            // change the times below to set default due date times
-			const dueTime12hrs = ["7:55am", "9:50am", "11:20am", "1:15pm"];
+			// change the times below to set default due date times
 
-			const dueTime24hrs = ["07:55", "09:50", "11:20", "13:15"];
+			/**
+			 * defaultTimes is an array of 24 hour times in HH:MM format
+			 * timeFormat is the default format for displaying times
+			 *   see https://day.js.org/docs/en/display/format for more info
+			 */
+
+			const defaultTimes = ['07:55', '09:50', '11:20', '13:15'];
+			const timeFormat = 'h:mma';
+
+			const today = dayjs().format('YYYY-MM-DD');
+			const displayTimes = defaultTimes.map(t =>
+				dayjs(`${today} ${t}`).format(timeFormat)
+			);
+
 
 			var dueDate_ids = [];
 
-            // adds existing due date input ids to array
+			// adds existing due date input ids to array
 			(function() {
 				$('input.DueDateInput[type=text').each(function() {
 					dueDate_ids.push(this.id);
@@ -35,23 +51,23 @@
 			})();
 
 
-            // loops through each existing due date input and adds all default due time input buttons to each due date input
+			// loops through each existing due date input and adds all default due time input buttons to each due date input
 			for (let i = 0; i < dueDate_ids.length; i++) {
 
-				for (let j = 0; j < dueTime12hrs.length; j++) {
+				for (let j = 0; j < displayTimes.length; j++) {
 
-					$(`.existing_date_input_row_${dueDate_ids[i]}`).append(`<input type="text" class="existing_date_custom_picker_${dueDate_ids[i]}_${[j]}" readonly="readonly" value="${dueTime12hrs[j]}" style="background: #f5f5f5;color: #2d3b45; margin: 10px 5px 0px 0px; max-width: 75px; border: 1px solid rgb(199, 205, 209); border-radius: 3px; transition: background-color 0.2s ease-in-out 0s; display: inline-block; position: relative; padding: 8px 2px; font-size: 1rem; line-height: 20px; text-align: center; vertical-align: middle; text-decoration: none; overflow: hidden; text-shadow: none; user-select: none; cursor: pointer !important;">`);
+					$(`.existing_date_input_row_${dueDate_ids[i]}`).append(`<input type="text" class="existing_date_custom_picker_${dueDate_ids[i]}_${[j]}" readonly="readonly" value="${displayTimes[j]}" style="background: #f5f5f5;color: #2d3b45; margin: 10px 5px 0px 0px; max-width: 75px; border: 1px solid rgb(199, 205, 209); border-radius: 3px; transition: background-color 0.2s ease-in-out 0s; display: inline-block; position: relative; padding: 8px 2px; font-size: 1rem; line-height: 20px; text-align: center; vertical-align: middle; text-decoration: none; overflow: hidden; text-shadow: none; user-select: none; cursor: pointer !important;">`);
 					$(`.existing_date_custom_picker_${dueDate_ids[i]}_${[j]}`).hover(function() {
 						$(this).css("background-color", "#e8e8e8");
 					}, function() {
 						$(this).css("background-color", "#f5f5f5");
 					});
 					$(`.existing_date_custom_picker_${dueDate_ids[i]}_${[j]}`).datepicker({
-						dateFormat: "yy-mm-dd " + dueTime24hrs[j],
+						dateFormat: "yy-mm-dd " + defaultTimes[j],
 						onSelect: function() {
 							$(this).closest('div').find('.DueDateInput').val($(`.existing_date_custom_picker_${dueDate_ids[i]}_${[j]}`).val()).trigger("change");
 							$(this).change(function() {
-								$(`.existing_date_custom_picker_${dueDate_ids[i]}_${[j]}`).val(dueTime12hrs[j]);
+								$(`.existing_date_custom_picker_${dueDate_ids[i]}_${[j]}`).val(displayTimes[j]);
 							});
 						}
 
@@ -62,7 +78,7 @@
 
 
 
-            // adds all default due time input buttons to each new due date input when +Add is clicked in Canvas
+			// adds all default due time input buttons to each new due date input when +Add is clicked in Canvas
 			$("#add_due_date").click(function() {
 
 
@@ -70,8 +86,8 @@
 
 					var lastRow_id_added = $('.Container__DueDateRow-item').find('input.DueDateInput[type=text]').last().attr("id");
 
-					for (let i = 0; i < dueTime12hrs.length; i++) {
-						$('.Container__DueDateRow-item').find('.DueDateInput__Container').last().append(`<input type="text" class="added_date_custom_picker_${lastRow_id_added}_${i}" readonly="readonly" value="${dueTime12hrs[i]}" style="background: #f5f5f5;color: #2d3b45; margin: 10px 5px 0px 0px; max-width: 75px; border: 1px solid rgb(199, 205, 209); border-radius: 3px; transition: background-color 0.2s ease-in-out 0s; display: inline-block; position: relative; padding: 8px 2px; font-size: 1rem; line-height: 20px; text-align: center; vertical-align: middle; text-decoration: none; overflow: hidden; text-shadow: none; user-select: none; cursor: pointer !important;">`);
+					for (let i = 0; i < displayTimes.length; i++) {
+						$('.Container__DueDateRow-item').find('.DueDateInput__Container').last().append(`<input type="text" class="added_date_custom_picker_${lastRow_id_added}_${i}" readonly="readonly" value="${displayTimes[i]}" style="background: #f5f5f5;color: #2d3b45; margin: 10px 5px 0px 0px; max-width: 75px; border: 1px solid rgb(199, 205, 209); border-radius: 3px; transition: background-color 0.2s ease-in-out 0s; display: inline-block; position: relative; padding: 8px 2px; font-size: 1rem; line-height: 20px; text-align: center; vertical-align: middle; text-decoration: none; overflow: hidden; text-shadow: none; user-select: none; cursor: pointer !important;">`);
 
 						$(`.added_date_custom_picker_${lastRow_id_added}_${i}`)
 							.hover(function() {
@@ -82,14 +98,14 @@
 
 						$(`.added_date_custom_picker_${lastRow_id_added}_${i}`)
 							.datepicker({
-								dateFormat: "yy-mm-dd " + dueTime24hrs[i],
+								dateFormat: "yy-mm-dd " + defaultTimes[i],
 								onSelect: function() {
 									$(this).closest('div').find('.DueDateInput')
 										.val($(`.added_date_custom_picker_${lastRow_id_added}_${i}`)
 											.val()).trigger("change");
 									$(this).change(function() {
 										$(`.added_date_custom_picker_${lastRow_id_added}_${i}`)
-											.val(dueTime12hrs[i]);
+											.val(displayTimes[i]);
 									});
 								}
 
